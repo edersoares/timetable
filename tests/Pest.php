b89fc6c40ad5857d1161f1ac4bbd1e5856d37228
Eder Soares <edersoares@me.com>
2025-09-12 16:45:10 -0300

    feat: executor

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,6 +2,16 @@
 
 declare(strict_types=1);
 
+use Portabilis\Timetable\Actions\Executor;
+use Portabilis\Timetable\Tests\Eloquent;
 use Portabilis\Timetable\Tests\TestCase;
+use Portabilis\Timetable\Tests\Tester;
 
 uses(TestCase::class)->in(__DIR__);
+uses(Tester::class)->in(__DIR__);
+uses(Eloquent::class)->in(__DIR__);
+
+function execute(string $action, array $data)
+{
+    return app(Executor::class)->execute($action, $data);
+}
