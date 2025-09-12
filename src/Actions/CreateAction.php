4fa5d259776995a4fcf2e80ba64ec18e44611dfd
Eder Soares <edersoares@me.com>
2025-09-12 16:24:12 -0300

    style: format

--- src/Actions/CreateAction.php
+++ src/Actions/CreateAction.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Portabilis\Timetable\Actions;
 
 use Illuminate\Database\Eloquent\Model;
--- src/Actions/DeleteAction.php
+++ src/Actions/DeleteAction.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Portabilis\Timetable\Actions;
 
 class DeleteAction extends OperationAction
